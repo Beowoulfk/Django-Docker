bbe6d0fb8e3b758578680edfc5bf47cca6acd200
Victor manuel torres J <victor.vt649@gmail.com>
2022-01-03 22:30:54 -0500

    se oculto la SECRET KEY de Django y se corrigio un comando de docker

--- Djangodocker/Mipagina/Mipagina/settings.py
+++ Djangodocker/Mipagina/Mipagina/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o#wz%k4me7a0#+8idc)@5jivc@q+(2u#$idz17*#7z(aq9f6-f'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
